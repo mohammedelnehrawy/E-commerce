60bd516225283dc18dfd9d859e8bac8784ce29d8
mohammedelnehrawy <mmdne8181@gmail.com>
2025-04-18 23:32:21 +0200

    freshCart

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'checkout/:id',
+
+    renderMode: RenderMode.Server,
+  },
 ];
--- src/server.ts
+++ src/server.ts
@@ -50,7 +50,7 @@ app.use('/**', (req, res, next) => {
 });
 
 /**
- * Start the server if this module is the main entry point.
+ * Start the server if this module is the main entry point. 
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
 if (isMainModule(import.meta.url)) {
